358701b509c381ca880577ace49a0556de4d63b6
Thales Lima <thaleslimadejesus@gmail.com>
2020-04-28 22:08:54 -0300

    Subida do site finalizado.

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    $('#Descricao1').hide();
+    $('#Descricao2').hide();
+    $('#Descricao3').hide();
+    $('#Descricao4').hide();
+    $('#Descricao5').hide();
+    $('#Descricao6').hide();
+
+    $('#btnVejaMais1').click(function(){
+        $('#Descricao1').toggle();
+    });
+    
+    $('#btnVejaMais2').click(function(){
+        $('#Descricao2').toggle();
+    });
+    
+    $('#btnVejaMais3').click(function(){
+        $('#Descricao3').toggle();
+    });
+    
+    $('#btnVejaMais4').click(function(){
+        $('#Descricao4').toggle();
+    });
+    
+    $('#btnVejaMais5').click(function(){
+        $('#Descricao5').toggle();
+    });
+    
+    $('#btnVejaMais6').click(function(){
+        $('#Descricao6').toggle();
+    });
+
+});
